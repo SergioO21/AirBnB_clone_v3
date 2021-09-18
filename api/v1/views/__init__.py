ce45d17a6529fbab2e5eb8604ee227a93dbb54e4
Rode1o <dumirola.dr@gmail.com>
2021-09-17 21:19:15 -0500

    changes API

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+""" Init module """
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+""" Index module """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """ Route that returns a json response"""
+    return jsonify({"status": "OK"})
