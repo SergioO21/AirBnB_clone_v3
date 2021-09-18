f812e2555e15e7d5d65464b4c3afbcbeb2bfcb53
Rode1o <dumirola.dr@gmail.com>
2021-09-17 22:46:05 -0500

    retrieve a dict stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,13 +6,15 @@ from api.v1.views import app_views
 from models import storage
 
 classes = {"amenities": "Amenity", "cities": "City",
-           "places": "Place", "reviews": "Review", "states": "State", "users": "User"}
+           "places": "Place", "reviews": "Review",
+           "states": "State", "users": "User"}
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """ Route that returns a json response"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
@@ -22,6 +24,7 @@ def stats():
         number = storage.count(value)
         new_dict2[key] = number
     return jsonify(new_dict2)
+
 
 if __name__ == "__main__":
     pass
