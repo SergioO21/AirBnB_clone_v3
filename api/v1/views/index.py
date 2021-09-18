5be95fa806ecc3a9ae3d2b0b614307705e0a88eb
Rode1o <dumirola.dr@gmail.com>
2021-09-17 22:27:31 -0500

    retrieve a dict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -10,5 +11,14 @@ def status():
     """ Route that returns a json response"""
     return jsonify({"status": "OK"})
 
-if __name__ == '__main__':
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """ Retrieves the number of each element by type"""
+    new_dict2 = {}
+    for key, value in classes.item:
+        number = storage.count(value)
+        new_dict2[key] = number
+    return jsonify(new_dict2)
+
+if __name__ == "__main__":
     pass
