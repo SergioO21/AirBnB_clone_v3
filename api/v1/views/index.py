f079df4d89ba373950fde0114bb08d35cb8f638a
Rode1o <dumirola.dr@gmail.com>
2021-09-17 21:41:57 -0500

    change API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Index module """
 
+
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -9,3 +10,6 @@ from api.v1.views import app_views
 def status():
     """ Route that returns a json response"""
     return jsonify({"status": "OK"})
+
+if __name__ == "__main__":
+    pass
